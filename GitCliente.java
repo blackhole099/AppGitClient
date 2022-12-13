419018f1587556f6fb701f637680c9d69b7de0c0
blackhole099 <118816306+blackhole099@users.noreply.github.com>
2022-12-13 13:25:43 +0100

    Add files via upload

---
+++ GitCliente.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entornos;
+
+/**
+ *git client describe el tipo de Git client elegido
+ * @author vilaloen
+ */
+public class GitCliente {
+
+    private String cliente;
+    private String version;
+    private String systemopen;
+    private String licencia;
+   public GitCliente(){
+   }
+   public String getcliente(){
+       return cliente;
+   }
+   public void setcliente(String cliente){
+       this.cliente=cliente;
+   }
+   public String getversion(){
+       return version;
+   }
+   public void setversion(String version){
+       this.version=version;
+   }
+   public String getsystemopen(){
+       return systemopen;
+   }
+   public void setsystemopen(String systemopen){
+       this.systemopen=systemopen;
+   }
+   public String getlicencia(){
+       return licencia;
+   }
+   public void setlicencia(String licencia){
+       this.licencia=licencia;
+   }
+    
+}
