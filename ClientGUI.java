5b5f8c801518366113b88a8d05c65c0cec0e67c2
blackhole099 <118816306+blackhole099@users.noreply.github.com>
2022-12-28 17:15:35 +0100

    Add files via upload

---
+++ ClientGUI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entornos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClientGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola ClientGUI");
+    }
+    
+}
